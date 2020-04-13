26fd37dcce8e6c063e12fd45d053129a7fa80efd
orangefruit21 <borovskih.05021999@gmail.com>
2020-04-14 01:26:06 +0300

    Created a save in the storage

--- js/main.js
+++ js/main.js
@@ -1,23 +1,49 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.addEventListener('submit', (event) => {
-//         event.preventDefault();
-//         let elem = document.createElement('p');
-//         elem.innerText=document.form.input.value;
-//         document.body.appendChild(elem);
-//     });
-// });
-
+// Инициализация хранилища, отрисовка сохраненных "задач", навешивание обработчиков создания и удаления
 $(function() {
     const store = new Store;
-    addRemoveListener();
+    store.todos.forEach(todo => {
+        createTodo('',todo.title, todo.description, todo.id);
+    });
+    addRemoveListener(store);
     $('.add-btn').click(function(e){
-        createTodo(e);
+        const title = document.addTodoForm.title.value;
+        const description = document.addTodoForm.description.value;
+        if(!title || !description) {
+            return false
+        }
+        createTodo(e, title, description);
+        title && description && store.addTodo(title,description);
         document.addTodoForm.title.value = "";
         document.addTodoForm.description.value = "";
-        addRemoveListener();
+        addRemoveListener(store);
     });
- });
+});
 
+// Функция для сборки "задачи" и размещеня на странице
+const createTodo = (e, title, description, id) => {
+    e && e.preventDefault();
+    $('.empty-todo') && $('.empty-todo').remove();
+    const todo = $('<article></article>');
+    const model = '<div class="controll-wrapper clearfix"><h3>'+title+'</h3><button class="delet-btn"><span></span></button><button class="suspend-btn"></button></div><p>'+description+'</p>';
+    todo.html(model);
+    todo[0].id = id;
+    $(".todo-list-wrapper").append(todo);
+};
+
+// Функция для навешивания слушателя удаления "задачи"
+const addRemoveListener = (store) => {
+    $('.delet-btn').click(function(e){
+        e.preventDefault();
+        const todoId = parseInt($(e.target).parents('article')[0].id);
+        store.deleteTodo(todoId);
+        $(e.target).parents('article').remove();
+        if ($('article').length == 0 && $('.empty-todo').length == 0) {
+            $(".todo-list-wrapper").append('<p class ="empty-todo">Список пуст...</p>');
+        }    
+    });
+};
+
+// Хранилище
 class Store {
     constructor(){
         this.todos = JSON.parse(localStorage.getItem('todos')) || [];
@@ -29,35 +55,16 @@ class Store {
             title: todoTitle,
             description: todoDescr
         }
-        this.todos.push(todo)
+        this.todos.push(todo);
+        this._save(this.todos);
     }
 
     deleteTodo(id){
-        this.todos = this.todos.filter(todo => todo.id !== id)
+        this.todos = this.todos.filter(todo => todo.id !== id);
+        this._save(this.todos);
     }
-};
 
-const createTodo = (e) => {
-    e.preventDefault();
-    const title = document.addTodoForm.title.value;
-    const description = document.addTodoForm.description.value;
-    if(!title || !description) {
-        return false
+    _save(todos){
+        localStorage.setItem('todos', JSON.stringify(todos));
     }
-    $('.empty-todo') && $('.empty-todo').remove();
-    const todo = $('<article></article>');
-    const model = '<div class="controll-wrapper clearfix"><h3>'+title+'</h3><button class="delet-btn"><span></span></button><button class="suspend-btn"></button></div><p>'+description+'</p>';
-    todo.html(model);
-    $(".todo-list-wrapper").append(todo);
-};
-
-const addRemoveListener = () => {
-    $('.delet-btn').click(function(e){
-        e.preventDefault();
-        $(e.target).parents('article').remove();
-        console.log($('article').length);
-        if ($('article').length == 0 && $('.empty-todo').length == 0) {
-            $(".todo-list-wrapper").append('<p class ="empty-todo">Список пуст...</p>');
-        }    
-    });
 };
